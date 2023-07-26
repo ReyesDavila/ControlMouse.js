683b0de2fc14abe7d99eb7dc39a8bbb2d0aae390
ReyesDavila <87891319+ReyesDavila@users.noreply.github.com>
2023-07-26 10:34:54 -0400

    Update ControlMouse.js

--- ControlMouse.js
+++ ControlMouse.js
@@ -4,40 +4,65 @@
 
 // Declara las variables
 var touchDevice = ('ontouchstart' in document.documentElement);
-var mouseX = 0;
-var mouseY = 0;
+var mouseXY = [];
+var mouse_x = 0;
+var mouse_y = 0;
+var mouse_count = 0;
+function floor(x) {return Math.floor(x)}
 
 function mouseMove(e) {
-  if (touchDevice){
-    mouseX = e.touches[0].clientX - document.documentElement.clientLeft;
-    mouseY = e.touches[0].clientY - document.documentElement.clientTop;
-  } else {
-    mouseX = e.clientX - document.documentElement.clientLeft;
-    mouseY = e.clientY - document.documentElement.clientTop;
-  }
+  if (touchDevice) {
+	mouseXY = [];
+	mouse_count = event.targetTouches.length;
+	for(index=0;index<mouse_count;index++)
+	{
+		mouseXY.push([floor(event.touches[index].clientX),floor(event.touches[index].clientY)]);
+	}
+   }
+   else{
+	mouseXY = [];
+	mouseXY.push([floor(event.clientX),floor(event.clientY)]);
+	mouse_count = mouseXY.length;
+	if(event.type == "mousedown"){touch_count = 1}
+	if(event.type == "mouseup"){touch_count = 0}
+       }
+
+	if(mouse_count){mouse_x = mouseXY[0][0];mouse_y = mouseXY[0][1]};
+	salida[0] = mouseXY;
 }
 
 // Añade el controlador de eventos
 if(touchDevice) {
-  document.addEventListener("touchmove", mouseMove, false);
-  document.addEventListener("touchstart", mouseMove, false);
+	document.addEventListener("touchstart", mouseMove, false);
+        document.addEventListener("touchmove", mouseMove, false);
+        document.addEventListener("touchend", mouseMove, false);
 }
 else {
-  document.addEventListener("mousemove", mouseMove, false);
-  document.addEventListener("mousedown", mouseMove, false);
+	document.addEventListener("mousedown", mouseMove, false);
+	document.addEventListener("mousemove", mouseMove, false);
+	document.addEventListener("mouseup", mouseMove, false);
 }
 
 
 // Función práctica para obtener la posición del mouse en un elemento
 function getMousePositionInElement(element) {
   // Obtiene la posición del elemento en píxeles.
-  const rect = element.getBoundingClientRect();
+const rect = element.getBoundingClientRect();
 
-  // Calcula la posición del mouse en el elemento.
-  const mouseXInElement = mouseX - rect.x;
-  const mouseYInElement = mouseY - rect.y;
+var mouseXInElement = null;
+var mouseYInElement = null;
 
+// Busca si un mouse está sobre el elemento y alcula la posición del mouse en el elemento.
+
+  for (const [mx, my] of mouseXY) {
+    if(mx>=rect.x && mx<=rect.x+rect.width && my>=rect.y && my<=rect.y+rect.height)
+	{
+         mouseXInElement = mx - rect.x;
+	 mouseYInElement = my - rect.y;
+	}
+  }
+
+  
   // Devuelve la posición del mouse en el elemento.
   return [mouseXInElement, mouseYInElement];
 }
-
